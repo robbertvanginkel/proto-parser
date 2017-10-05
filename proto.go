0f9581c998cbe759a3f1968f20ec8b4c5db281b1
Robbert Van Ginkel <robbert@uber.com>
2017-10-05 18:04:00 +0200

    identifiers on Pos tag

--- proto.go
+++ proto.go
@@ -1,3 +1,7 @@
+//
+// A parser for protobuf 3 files
+// https://developers.google.com/protocol-buffers/docs/reference/proto3-spec
+//
 package main
 import (
 	"os"
@@ -23,20 +27,20 @@ type Constant struct {
 }
 
 type Import struct {
-	Pos  lexer.Position
-	Kind string `"import" @["weak" | "public"]`
-	Name string `@String ";"`
+	Pos  lexer.Position `"import"`
+	Kind string         `@["weak" | "public"]`
+	Name string         `@String ";"`
 }
 
 type Package struct {
-	Pos  lexer.Position
-	Name string `"package" @Ident {@"." @Ident} ";"`
+	Pos  lexer.Position `"package"`
+	Name string         `@Ident {@"." @Ident} ";"`
 }
 
 type Option struct {
-	Pos      lexer.Position
-	Name     string    `"option" (@Ident | "(" @Ident {@"." @Ident} ")") { "." @Ident }`
-	Constant *Constant `"=" @@ ";"`
+	Pos      lexer.Position `"option"`
+	Name     string         `(@Ident | "(" @Ident {@"." @Ident} ")") { "." @Ident }`
+	Constant *Constant      `"=" @@ ";"`
 }
 
 type ValueOption struct {
@@ -53,10 +57,10 @@ type EnumField struct {
 }
 
 type Enum struct {
-	Pos     lexer.Position
-	Name    string       `"enum" @Ident "{"`
-	Options []*Option    `{ @@  `
-	Cases   []*EnumField `| @@ | ";" } "}"`
+	Pos     lexer.Position `"enum"`
+	Name    string         `@Ident "{"`
+	Options []*Option      `{ @@  `
+	Cases   []*EnumField   `| @@ | ";" } "}"`
 }
 
 type Field struct {
@@ -77,14 +81,14 @@ type OneofField struct {
 }
 
 type Oneof struct {
-	Pos    lexer.Position
-	Name   string        `"oneof" @Ident "{"`
-	Fields []*OneofField ` { @@ } "}"`
+	Pos    lexer.Position `"oneof"`
+	Name   string         `@Ident "{"`
+	Fields []*OneofField  ` { @@ } "}"`
 }
 
 type MapField struct {
-	Pos          lexer.Position
-	KeyType      string         `"map" "<" @("int32" | "int64" | "uint32" | "uint64" | "sint32" | "sint64" | "fixed32" | "fixed64" | "sfixed32" | "sfixed64" | "bool" | "string") ","`
+	Pos          lexer.Position `"map"`
+	KeyType      string         `"<" @("int32" | "int64" | "uint32" | "uint64" | "sint32" | "sint64" | "fixed32" | "fixed64" | "sfixed32" | "sfixed64" | "bool" | "string") ","`
 	ValueType    string         `@Ident {@"." @Ident} ">"`
 	Name         string         `@Ident`
 	FieldNumber  int            `"=" @Int`
@@ -92,57 +96,55 @@ type MapField struct {
 }
 
 type Range struct {
-	Pos   lexer.Position
 	From  int  `@Int`
 	To    int  `["to" (@Int | `
 	ToMax bool `        @"max")]`
 }
 
 type Reserved struct { // not completley correct as it can handle both now
-	Pos      lexer.Position
-	reserved string   `"reserved"`
-	Fields   []string `[@String {"," @String}]`
-	Ranges   []*Range `[@@ { "," @@ }] ";"`
+	Pos    lexer.Position `"reserved"`
+	Fields []string       `[@String {"," @String}]`
+	Ranges []*Range       `[@@ { "," @@ }] ";"`
 }
 
 type Message struct {
-	Pos       lexer.Position
-	Name      string      `"message" @Ident "{"`
-	Enums     []*Enum     `{ @@ `
-	Messages  []*Message  ` | @@ `
-	MapFields []*MapField ` | @@`
-	Options   []*Option   ` | @@`
-	Oneofs    []*Oneof    ` | @@ `
-	Reserved  []*Reserved ` | @@`
-	Fields    []*Field    ` | @@ } "}"`
+	Pos       lexer.Position `"message"`
+	Name      string         `@Ident "{"`
+	Enums     []*Enum        `{ @@ `
+	Messages  []*Message     ` | @@ `
+	MapFields []*MapField    ` | @@`
+	Options   []*Option      ` | @@`
+	Oneofs    []*Oneof       ` | @@ `
+	Reserved  []*Reserved    ` | @@`
+	Fields    []*Field       ` | @@ } "}"`
 }
 
 type Rpc struct {
-	Pos           lexer.Position
-	Name          string    `"rpc" @Ident "("`
-	RequestStream bool      `[@"stream"]`
-	RequestType   string    `@(["."] Ident { "." Ident }) ")"`
-	ReturnStream  bool      `"returns" "(" [@"stream"]`
-	ReturnType    string    `@(["."] Ident { "." Ident }) ")"`
-	Options       []*Option `(("{" { @@ } "}") | ";")`
+	Pos           lexer.Position `"rpc"`
+	Name          string         `@Ident "("`
+	RequestStream bool           `[@"stream"]`
+	RequestType   string         `@(["."] Ident { "." Ident }) ")"`
+	ReturnStream  bool           `"returns" "(" [@"stream"]`
+	ReturnType    string         `@(["."] Ident { "." Ident }) ")"`
+	Options       []*Option      `(("{" { @@ } "}") | ";")`
 }
 
 type Service struct {
-	Pos     lexer.Position
-	Name    string    `"service" @Ident "{"`
-	Options []*Option `{ @@ `
-	Rpcs    []*Rpc    ` | @@ } "}"`
+	Pos     lexer.Position `"service"`
+	Name    string         `@Ident "{"`
+	Options []*Option      `{ @@ `
+	Rpcs    []*Rpc         ` | @@ } "}"`
 }
 
 type Proto struct {
-	Pos      lexer.Position
-	Syntax   string     `"syntax" "=" @String ";"`
-	Imports  []*Import  `{ @@ `
-	Packages []*Package `  | @@ `
-	Options  []*Option  `  | @@ `
-	Enums    []*Enum    `  | @@ `
-	Services []*Service `  | @@ `
-	Messages []*Message `  | @@ }`
+	Pos      lexer.Position `"syntax" "=" `
+	Syntax   string         `@String ";"`
+	Imports  []*Import      `{ @@ `
+	Packages []*Package     `  | @@ `
+	Options  []*Option      `  | @@ `
+	Enums    []*Enum        `  | @@ `
+	Services []*Service     `  | @@ `
+	Messages []*Message     `  | @@ }`
 }
 
 func main() {
